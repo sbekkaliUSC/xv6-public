c40bf823ac79b360ac74b8dcb17cd69b33dabc49
Saleem Bekkali <bekkali@usc.edu>
2025-03-17 06:47:23 +0000

    Enhance race condition demonstration by adding file synchronization using a condition variable

--- race.c
+++ race.c
@@ -2,11 +2,13 @@
 #include "stat.h"
 #include "user.h"
 #include "condvar.h"
+#include "fcntl.h"
 
 // We want Child 1 to execute first, then Child 2, and finally Parent.
 int main()
 {
   struct condvar cv;
+  int fd = open("flag", O_RDWR | O_CREATE);
   init_lock(&cv.lk);
   int pid = fork(); // fork the first child
   if (pid < 0)
@@ -18,6 +20,7 @@ int main()
     sleep(5);
     printf(1, "Child 1 Executing\n");
     lock(&cv.lk);
+    write(fd, "done", 4);
     cv_signal(&cv);
     unlock(&cv.lk);
   }
@@ -31,7 +34,15 @@ int main()
     else if (pid == 0)
     {
       lock(&cv.lk);
-      cv_wait(&cv);
+      struct stat stats;
+      fstat(fd, &stats);
+      printf(1, "file size = %d\n", stats.size);
+      while (stats.size <= 0)
+      {
+        cv_wait(&cv);
+        fstat(fd, &stats);
+        printf(1, "file size = %d\n", stats.size);
+      }
       unlock(&cv.lk);
       printf(1, "Child 2 Executing\n");
     }
@@ -46,5 +57,7 @@ int main()
       printf(1, "Parent exiting.\n");
     }
   }
+  close(fd);
+  unlink("flag");
   exit();
 }
